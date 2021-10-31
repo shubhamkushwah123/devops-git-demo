7244848ee231f90f9fb4a840b37cbe7f9044ea0f
shubhamkushwah123 <shubhamkushwah123@xyz.com>
2021-10-31 16:50:54 +0000

    3rd commit

--- Demo.java
+++ Demo.java
@@ -1,6 +1,6 @@
 public class Demo{
 	public static void main(Strings args[]){
 		System.out.println("Hello World");
-		System.out.println("Learning git");
+		System.out.println("Learning git and DevOps");
 	}
 }
